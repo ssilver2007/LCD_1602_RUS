9c25b1fa4c03b0503fd6d31267c38cb90c14298c
ssilver2007 <ssilver2007@rambler.ru>
2015-12-26 22:04:33 +0300

    Create HelloWorld.ino

---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,20 @@
+#include "LCD_1602_RUS.h"
+
+LCD_1602_RUS lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(2, 0);
+  lcd.print(L"РўРµРїРµСЂСЊ РјРѕР¶РЅРѕ");
+  lcd.setCursor(3, 1);
+  lcd.print(L"РЅР° СЂСѓСЃСЃРєРѕРј");
+
+}
+
+
+void loop()
+{
+}
