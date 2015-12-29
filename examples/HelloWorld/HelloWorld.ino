05c2ef5bcaf12687cf720c84450a1175744237d9
ssilver2007 <ssilver2007@rambler.ru>
2015-12-29 20:15:35 +0300

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -2,18 +2,15 @@
 
 LCD_1602_RUS lcd(0x27, 16, 2);
 
-const wchar_t str1[] PROGMEM = L"Теперь можно";
-const wchar_t str2[] PROGMEM = L"по-русски";
-
 void setup()
 {
   lcd.init();                      // initialize the lcd
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(2, 0);
-  lcd.print(str1);
+  lcd.print(L"Теперь можно");
   lcd.setCursor(3, 1);
-  lcd.print(str2);
+  lcd.print(L"по-русски");
 
 }
 
