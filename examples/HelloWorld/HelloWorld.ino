8722bbfc35c84bdce7648d20da0cfd195ee287d8
Sergey Sirotkin <ssilver2007@rambler.ru>
2019-10-10 21:08:51 +0300

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -4,13 +4,16 @@ LCD_1602_RUS lcd(0x27, 16, 2);
 
 void setup()
 {
+  String str;
+  str = "по-русски";
+
   lcd.init();                      // initialize the lcd
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(2, 0);
-  lcd.print(L"Теперь можно");
+  lcd.print("Теперь можно");
   lcd.setCursor(3, 1);
-  lcd.print(L"по-русски");
+  lcd.print(str);
 
 }
 
